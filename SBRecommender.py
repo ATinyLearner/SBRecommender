3a55dd7e765f43f3d8f4083e8a1bde98854a6648
Avik Ghosh <36378913+ATinyLearner@users.noreply.github.com>
2022-02-26 10:58:54 +0530

    updated app.py

--- SBRecommender.py
+++ SBRecommender.py
@@ -1,6 +1,6 @@
 # All imports
 import datetime
-from pycaret.classification import load_model, predict_model
+from pycaret.classification import *
 import streamlit as st
 import pandas as pd
 import numpy as np
